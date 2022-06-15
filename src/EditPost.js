e36e9d8ee854b0a4b212d9b3a2516803761e411e
Bron200089 <200089@virtualwindow.co.za>
2022-06-15 19:17:18 +0200

    Assessment Two Complete!
    completed the assessment.

--- src/EditPost.js
+++ src/EditPost.js
@@ -2,16 +2,43 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 import axios from 'axios';
 
-const EditPost = () => {
+const EditPost = (props) => {
 
+  const [updatedPost, setUpdatedPost] = useState({
+    newMessage: props.origional ,
+    id: props.id
+  })
+
+  const closeModal = () => {
+    props.rerender();
+  }
+
+  useEffect(() => {
+    document.getElementById('updateText').innerHTML = props.origional;
+  }, []);
+
+  const handleChange = (e) => {
+    let value = e.target.value;
+    setUpdatedPost({...updatedPost, newMessage:value});
+  }
+
+  const updatePost = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:8888/assessmentTwoApi/editPost.php', updatedPost)
+    .then((res)=>{
+      let data = res.data;
+      props.upRender(true);
+      props.rerender();
+    });
+  }
 
   return (
     <div className='modal'>
       <form>
         <h1>Made a Mistake? Edit that shit!</h1>
-        <p>Close Modal</p>
-        <textarea id='updateText' placeholder='Edit Post Message'/>
-        <button type='submit'>Edit this post</button>
+        <p onClick={closeModal}>Close Modal</p>
+        <textarea id='updateText' placeholder='Edit Post Message' onChange={handleChange}/>
+        <button type='submit' onClick={updatePost}>Edit this post</button>
       </form>
     </div>
   )
